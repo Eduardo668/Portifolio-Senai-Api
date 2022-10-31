cce45df8bb86a57916fd04e2016c78ae0406ed69
Eduardo668 <eduardosehn20@gmail.com>
2022-10-31 09:23:03 -0300

    Cross Origin

--- src/main/java/br/com/senaiportifolio/routes/TrabalhoController.java
+++ src/main/java/br/com/senaiportifolio/routes/TrabalhoController.java
@@ -5,15 +5,7 @@ import java.util.List;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.senaiportifolio.models.Trabalho;
@@ -21,6 +13,7 @@ import br.com.senaiportifolio.services.TrabalhoServiceImpl;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(value = "http://localhost:3000")
 public class TrabalhoController {
 
     private final TrabalhoServiceImpl trabalhoService;
